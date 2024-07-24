adec533608fea6fac1cec2f345694664d445b5fe
Jakub Turski <yacoob@ftml.net>
2024-07-24 23:46:13 +0100

    refactor: use new Extractors in beancount_aib

--- beancount_aib/extractors.py
+++ beancount_aib/extractors.py
@@ -1,68 +1,132 @@
 """Extractors for AIB transactions."""
+# ruff: noqa: ERA001
+# pyright: reportArgumentType=false
 
 from beancount_tx_cleanup.cleaner import (
-    CLEANUP,
-    TAG_DESTINATION,
-    OldE,
-    OldExtractors,
+    C,
+    E,
+    Extractors,
+    M,
+    P,
+    T,
 )
 
-AIB_EXTRACTORS: OldExtractors = {
-    # transaction timestamp
-    'time': (OldE(r'(?i)(time)? *(\d\d:\d\d)$', value=r'\g<2>'),),
-    # amount of foreign currency
-    'foreign-amount': (OldE(r' ([\d.]+) [A-Z]{3}@ [\d.]+', replacement=r'\g<0>'),),
-    # IDs that change for every instance of similar transactions
-    'id': (
-        OldE(r' *(IE\d+)'),
-        OldE(r'RYANAIR +(.+)', replacement='Ryanair', value=r'\g<1>'),
-        OldE(r'FREENOW\*(?=.*\d)([A-Z\d-]+)', replacement='FreeNow', value=r'\g<1>'),
+_exs: Extractors = []
+AIB_EXTRACTORS = _exs
+
+# remove leading and trailing stars
+_exs.append(E(r=r'(^\*|\*$)', actions=[C]))
+_exs.append(E(r=r'( \*|\* )', actions=[P(v=' ')]))
+
+# transaction timestamp
+_exs.append(
+    E(r=r'(?i)(time)? *(\d\d:\d\d)$', actions=[M(n='time', v=r'\2'), C]),
+)
+
+# foreign currency transaction
+_exs.append(
+    E(
+        r=r' ([\d.]+) ([A-Z]{3})@ [\d.]+',
+        actions=[M(n='foreign-amount'), T(v=r'\2'), C],
     ),
-    TAG_DESTINATION: (
-        # foreign currency symbol
-        OldE(r' [\d.]+ ([A-Z]{3})@ [\d.]+'),
-        # transaction flavour
-        # https://aib.ie/our-products/current-accounts/keeping-track-of-your-transactions
-        OldE(r'(?i)^(vd[apc]|op/|atm|pos|mobi|inet|d/d|atmldg|ms[ap]|)[- ]',
-          translation={
-            'atm': 'atm-aib',
-            'atmldg': 'atm-lodgement',
-            'd/d': 'direct-debit',
-            'inet': 'online-interface',
-            'mobi': 'app',
-            'msa': 'atm',
-            'msp': 'point-of-sale',
-            'op/': 'direct-debit',
-            'pos': 'point-of-sale',
-            'vda': 'atm',
-            'vdc': 'contactless',
-            'vdp': 'point-of-sale',
-          }),
+)
+
+# IDs that change for every instance of similar transactions
+M_ID = M(n='id')
+_exs.append(E(r=r' *(IE\d+)', actions=[M_ID, C]))
+_exs.append(E(r=r'RYANAIR +(.+)', actions=[M_ID, P(v='Ryanair')]))
+_exs.append(
+    E(r=r'FREENOW\*(?=.*\d)([A-Z\d-]+)', actions=[M_ID, P(v='FreeNow')]),
+)
+
+# transaction flavour
+# https://aib.ie/our-products/current-accounts/keeping-track-of-your-transactions
+_exs.append(
+    E(
+        r=r'(?i)^(vd[apc]|op/|atm|pos|mobi|inet|d/d|atmldg|ms[ap]|)[- ]',
+        actions=[
+            T(
+                translation={
+                    'atm': 'atm-aib',
+                    'atmldg': 'atm-lodgement',
+                    'd/d': 'direct-debit',
+                    'inet': 'online-interface',
+                    'mobi': 'app',
+                    'msa': 'atm',
+                    'msp': 'point-of-sale',
+                    'op/': 'direct-debit',
+                    'pos': 'point-of-sale',
+                    'vda': 'atm',
+                    'vdc': 'contactless',
+                    'vdp': 'point-of-sale',
+                },
+            ),
+            C,
+        ],
     ),
-    # payment processing companies
-    'payment-processor': (
-        OldE(r'(?i)^(paypal|sumup|sq|sp|zettle)[ _]', transformer=lambda m: m.lower(),
-          translation={
-            'sq': 'square',
-            'sp': 'stripe',
-          }),
-        OldE(r'(?i)^google +\b(?!google|cloud|commerce|domain|ireland|music|payment|play|servic|svcs|store|youtub|voic)(.+)',
-          replacement=r'\g<1>', value='google'),
-        OldE(r'(?i)^google +\b(payment|play|servic)', replacement=r'\g<0>', value='google'),
+)
+
+# payment processing companies
+TAG = 'payment-processor'
+_exs.append(
+    E(
+        r=r'(?i)^(paypal|sumup|sq|sp|zettle)[ _]',
+        actions=[
+            M(
+                n=TAG,
+                transformer=lambda m: m.lower(),
+                translation={
+                    'sq': 'square',
+                    'sp': 'stripe',
+                },
+            ),
+            C,
+        ],
     ),
-    CLEANUP: (
-        # remove the date strings, unless it's describing a quarterly fee transaction
-        OldE(r'(?<!TO )\d\d[A-Z]{3}\d\d *'),
-        # behold the museum of Amazon payment strings >_<;
-        OldE(r'(?i)^(www.)?amazon((\.co)?\.[a-z]{2,3})(.*)', replacement=lambda m: r'Amazon' + m.group(2).lower() + m.group(4)),
-        OldE(r'(?i)^amazon prime.*', replacement='Amazon Prime'),
-        OldE(r'(?i)^amazon [\d-]+', replacement=r'Amazon'),
-        OldE(r'(?i)^(amazon[^*]+)\*.*', replacement=r'\g<1>'),
+)
+M_GOOG = M(n=TAG, v='google')
+_exs.append(
+    E(
+        r=r'(?i)^google +\b(?!google|cloud|commerce|domain|ireland|music|payment|play|servic|svcs|store|youtub|voic)(.+)',
+        actions=[
+            M_GOOG,
+            P(v=r'\1'),
+        ],
     ),
-    'location': (
-        # An assortment of payees that routinely have a branch name present
-        OldE('(?i)^(applegreen|boi|centra|circle k|dunnes|eurospar|gamestop|mcdonalds|michie sushi|pablo picante|park rite|penneys|pizza hut|polonez|spar|starbucks|supervalu|topaz|ubl|ulster bank|wh smith|zabka) +(.+)$',
-          replacement=lambda m: m.group(1).upper() if len(m.group(1)) <= 3 else m.group(1).capitalize(),  # noqa: PLR2004
-          value=r'\g<2>', transformer=lambda s: s.lower()),
+)
+_exs.append(
+    E(
+        r=r'(?i)^google +\b(payment|play|servic)',
+        actions=[M_GOOG, P(v=r'\0')],
     ),
-}  # fmt: skip
+)
+
+# remove the date strings, unless it's describing a quarterly fee transaction
+_exs.append(E(r=r'(?<!TO )\d\d[A-Z]{3}\d\d *', actions=[C]))
+
+
+# behold the museum of Amazon payment strings >_<;
+_exs.append(
+    E(
+        r=r'(?i)^(www.)?amazon((\.co)?\.[a-z]{2,3})(.*)',
+        actions=[P(v=lambda m: r'Amazon' + m.group(2).lower() + m.group(4))],
+    ),
+)
+_exs.append(E(r=r'(?i)^amazon prime.*', actions=[P(v='Amazon Prime')]))
+_exs.append(E(r=r'(?i)^amazon [\d-]+', actions=[P(v='Amazon')]))
+_exs.append(E(r=r'(?i)^(amazon[^*]+)\*.*', actions=[P(v=r'\g<1>')]))
+
+# An assortment of payees that routinely have a branch name present
+_exs.append(
+    E(
+        r='(?i)^(applegreen|boi|centra|circle k|dunnes|eurospar|gamestop|mcdonalds|michie sushi|pablo picante|park rite|penneys|pizza hut|polonez|spar|starbucks|supervalu|topaz|ubl|ulster bank|wh smith|zabka) +(.+)$',
+        actions=[
+            M(n='location', v=r'\2', transformer=lambda s: s.lower()),
+            P(
+                v=lambda m: m.group(1).upper()
+                if len(m.group(1)) <= 3
+                else m.group(1).capitalize(),
+            ),
+        ],
+    ),
+)
